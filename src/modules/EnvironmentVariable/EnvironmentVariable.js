b595db03338547194fbac3108e2d2a5cfe6c37d5
ramraj saini <ramrajsainiiitd@gmail.com>
2024-05-05 23:55:42 +0530

    local storage added

--- src/modules/EnvironmentVariable/EnvironmentVariable.js
+++ src/modules/EnvironmentVariable/EnvironmentVariable.js
@@ -3,23 +3,53 @@ import classes from './EnvironmentVariable.module.css';
 import PlusIcon from '../../assets/PlusIcon.svg';
 import DownloadIcon from '../../assets/downloadIcon.svg';
 import EnvironmentVariableUploadFile from './EnvironmentVariableUploadFile';
+import EnvironmentVariableEdit from './EnvironmentVariableEdit';
 
 function EnvironmentVariable() {
-function handleClick() {
-  
-}
+  const storedEnvironmentVariable = localStorage.getItem('Item');
+  const storedData = storedEnvironmentVariable
+    ? JSON.parse(storedEnvironmentVariable)
+    : [];
+  const dataArray = Array.isArray(storedData)
+    ? storedData
+    : Object.values(storedData);
 
+    console.log("stored data ",dataArray);
   return (
     <div className={classes.main}>
       <div>
         <h2>Environment variable</h2>
         <span>
-          <button onClick={handleClick}><img src={PlusIcon} alt="Plus Icon" /></button>
-          <button><img src={DownloadIcon} alt="Download Icon" /></button>
+          <button>
+            <img src={PlusIcon} alt="Plus Icon" />
+          </button>
+          <button>
+            <img src={DownloadIcon} alt="Download Icon" />
+          </button>
         </span>
       </div>
-      <span>No environment variables created.</span>
-      <EnvironmentVariableUploadFile/>
+      {dataArray.length > 0 ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dataArray.map((item, index) => (
+              <tr key={index}>
+                <td>{item.name}</td>
+                <td>{item.value}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <span>No environment variables created.</span>
+      )}
+      <EnvironmentVariableUploadFile />
+      <EnvironmentVariableEdit />
     </div>
   );
 }
--- src/modules/EnvironmentVariable/EnvironmentVariableEdit.js
+++ src/modules/EnvironmentVariable/EnvironmentVariableEdit.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './EnvironmentVariableEdit.module.css';
 
 const EnvironmentVariableEdit = () => {
+  const [AddVariable, setAddVariable] = useState({
+    name: '',
+    value: 0,
+  });
+  const [name, setName] = useState('');
+  const [value, setValue] = useState(0);
+  function handleClick() {
+    setAddVariable({
+      ...AddVariable,
+      name: name,
+      value: value,
+    });
+  }
+  const data = JSON.stringify(AddVariable);
+  localStorage.setItem('Item',data);
   return (
     <div className={classes.main}>
       Name:
-      <input type="text" />
-      Value: <input type="number" />
+      <input onChange={(e) => setName(e.target.value)} type="text" />
+      Value: <input onChange={(e) => setValue(e.target.value)} type="number" />
       <button>Delete</button>
       <button>Cancle</button>
-      <button>Add</button>
+      <button onClick={handleClick}>Add</button>
     </div>
   );
 };
